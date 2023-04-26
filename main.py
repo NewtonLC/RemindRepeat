0fbd925ea51a96e47344b458313a262cc74139df
NewtonLC <55558755+NewtonLC@users.noreply.github.com>
2023-04-26 19:21:58 +0000

    Outlined basic functions & Progress Log

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 '''
 
-Create a system that takes an item and how often it should be purchased(in days).
+Create a system that takes a task and how often it should be repeated(numDays)
 
-Idfk yet
+Create a method that marks when the user "does" the task:
+    Marks down the date and keeps track of it
+
+Create a method that tells the user all tasks to complete.
+    Lists every task, the date frequency, and how long it's been since last completion
+
+Create a method that lists all tasks whose deadlines are due within x days.
+    x depends on user input
+
+Create a method that adds a task.
+  Struct?
+    TaskName
+    DateFrequency
+    CompletionDateTimes
+
+Create a method that removes a task.
 
 '''
